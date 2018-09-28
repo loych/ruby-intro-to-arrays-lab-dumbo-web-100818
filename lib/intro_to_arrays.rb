bb433e346bca24de5872005e02aa34bc52d374a8
IS Choi <insil321@hotmail.com>
2018-09-28 14:38:33 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -20,3 +20,12 @@ end
 
 def first_element_with_array_methods(array)
   array.first 
+end
+
+def last_element_with_array_methods(array)
+  array.last
+end
+
+def length_of_array(array)
+  array.length
+end
